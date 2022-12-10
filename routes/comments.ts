7d8dcfdc4993aa874bad51ab08f74e59f2d47e40
Chihhung Jen <jenchihhung7724@gmail.com>
2022-12-10 17:44:08 +0800

    added test for GET /api/comments/:id

--- routes/comments.ts
+++ routes/comments.ts
@@ -8,4 +8,10 @@ router.get("/",async (req,res)=>{
     res.send(result);
 });
 
+router.get("/:id",async (req,res)=>{
+    const id = Number(req.params.id);
+    const result = await Comment.findOne(id);
+    res.send(result);
+});
+
 export default router;
--- tests/intergration/comments.test.ts
+++ tests/intergration/comments.test.ts
@@ -16,7 +16,8 @@ describe("GET /api/comments",()=>{
         await con.query(dropTableSql);
         await con.query(createTableCommentsSql);
         let insetSql = "INSERT INTO comments (postId,author,email,content) ";
-            insetSql += "VALUES (1,'author','email','content')";
+        insetSql += "VALUES (1,'author1','email1','content1'),";
+        insetSql += "(2,'author2','email2','content2')";
         await con.query(insetSql);
     });
 
@@ -28,14 +29,27 @@ describe("GET /api/comments",()=>{
         await con.end();
     });
 
-    it("should return status code 200",async ()=>{
-        const res = await request(server).get("/api/comments");
-        expect(res.status).toBe(200);
-    })
+    describe("GET /api/commejnts",()=>{
+        it("should return status code 200",async ()=>{
+            const res = await request(server).get("/api/comments");
+            expect(res.status).toBe(200);
+        })
+    
+        it("should return 2 comment",async ()=>{
+            const res = await request(server).get("/api/comments");
+            expect(res.body.length).toBe(2);
+        })
+    });
 
-    it("should return 1 comment",async ()=>{
-        const res = await request(server).get("/api/comments");
-        console.log(res.body)
-        expect(res.body.length).toBe(1);
-    })
+    describe("GET /api/commejnts/:id",()=>{
+        it("should return 1 comment",async ()=>{
+            const res = await request(server).get("/api/comments/1");
+            expect(res.body[0]).toMatchObject({
+                postId:1,
+                author:"author1",
+                email:"email1",
+                content:"content1"
+            });
+        })
+    });
 })
